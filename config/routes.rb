e20cf076d9fb5d0ed13604116936c7e104485e10
positivelyjon <positivelyjon@gmail.com>
2024-04-07 02:08:10 +0800

    Fix missing controller key

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :destroy, :index]
   get 'users/:id/goals', to: 'users#generate_goals'
-  get ''
   post 'signup', to: 'users#create'
   post 'login', to: 'users#login'
 end
