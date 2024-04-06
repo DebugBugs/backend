698cfc9100625bf41a9ba417336295d73b8bae90
positivelyjon <positivelyjon@gmail.com>
2024-04-07 01:46:36 +0800

    Create Render config files

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
-gem "mysql2"
+gem "pg", "~> 1.1"
 
 gem "jwt"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :destroy, :index]
-  get 'users/:id/use-energy', to: 'users#use_energy'
+  get 'users/:id/goals', to: 'users#generate_goals'
+  get ''
   post 'signup', to: 'users#create'
   post 'login', to: 'users#login'
 end
